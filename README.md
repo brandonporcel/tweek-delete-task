6bae892070f42334347171b2b09a1f9d27b32187
brandon <brandon7.7porcel@gmail.com>
2023-11-25 12:32:34 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# instagram-block
+chrome extension
+
+hide reels and suggested posts
+
+![image](https://github.com/brandonporcel/instagram-block/assets/66080281/b9f9e202-e872-43ce-856d-167a08a493c7)
+![image](https://github.com/brandonporcel/instagram-block/assets/66080281/5cb7aa3f-dc5b-442a-82c6-030f68ab5696)
---
+++ main.js
@@ -0,0 +1,37 @@
+document.addEventListener("click", async (e) => {
+  if (e.ctrlKey) {
+    const taskSelector =
+      'div[aria-describedby="rbd-hidden-text-0-hidden-text-0"]';
+    const selectedTask = e.target.closest(taskSelector);
+
+    if (!selectedTask) return;
+
+    try {
+      const deleteButton = await waitForElement(".Task_action__uspJX");
+      deleteButton.click();
+    } catch (error) {
+      console.error("Error waiting for delete button:", error);
+    }
+  }
+});
+
+function waitForElement(selector, timeout = 5000) {
+  return new Promise((resolve, reject) => {
+    const startTime = Date.now();
+
+    function checkElement() {
+      const element = document.querySelector(selector);
+      if (element) {
+        resolve(element);
+      } else if (Date.now() - startTime > timeout) {
+        reject(
+          new Error(`Timeout waiting for element with selector: ${selector}`)
+        );
+      } else {
+        setTimeout(checkElement, 100);
+      }
+    }
+
+    checkElement();
+  });
+}
